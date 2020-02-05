b5c55e35d05f46d8f9b3bffea71265fe7335e406
Aaron Ladage <aladage@degdigital.com>
2020-02-05 13:07:05 -0600

    manifest cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,8 +46,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `KC Rocks`,
+        short_name: `KC Rocks`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
