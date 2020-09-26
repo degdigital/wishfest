47bb2a5f42e2fcd6b0603071126868b23f6c5a82
Aaron Ladage <aladage@degdigital.com>
2020-09-26 11:19:21 -0500

    updated countdown date

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,52 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
 
 const calculateTimeLeft = () => {
-    const difference = +new Date('2020-11-06') - +new Date();
-    let timeLeft = {};
+  const difference = +new Date("2021-06-18") - +new Date()
+  let timeLeft = {}
 
-    if (difference > 0) {
-        timeLeft = {
-            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-            minutes: Math.floor((difference / 1000 / 60) % 60),
-            seconds: Math.floor((difference / 1000) % 60)
-        };
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
     }
-    return timeLeft;
-};
+  }
+  return timeLeft
+}
 
 const Countdown = ({ siteTitle }) => {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
 
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft())
+    }, 1000)
+  })
 
-    useEffect(() => {
-        setTimeout(() => {
-            setTimeLeft(calculateTimeLeft());
-        }, 1000);
-    });
-
-    return (
-        <section className="countdown">
-            <div className="page-wrapper page-wrapper--narrow">
-                <div className="countdown__item">
-                    <div className="countdown__value">{timeLeft.days}</div>
-                    <div className="countdown__label">days</div>
-                </div>
-                <div className="countdown__item">
-                    <div className="countdown__value">{timeLeft.hours}</div>
-                    <div className="countdown__label">hours</div>
-                </div>
-                <div className="countdown__item">
-                    <div className="countdown__value">{timeLeft.minutes}</div>
-                    <div className="countdown__label">minutes</div>
-                </div>
-                <div className="countdown__item">
-                    <div className="countdown__value">{timeLeft.seconds}</div>
-                    <div className="countdown__label">seconds</div>
-                </div>
-            </div>
-        </section>
-    );
-};
+  return (
+    <section className="countdown">
+      <div className="page-wrapper page-wrapper--narrow">
+        <div className="countdown__item">
+          <div className="countdown__value">{timeLeft.days}</div>
+          <div className="countdown__label">days</div>
+        </div>
+        <div className="countdown__item">
+          <div className="countdown__value">{timeLeft.hours}</div>
+          <div className="countdown__label">hours</div>
+        </div>
+        <div className="countdown__item">
+          <div className="countdown__value">{timeLeft.minutes}</div>
+          <div className="countdown__label">minutes</div>
+        </div>
+        <div className="countdown__item">
+          <div className="countdown__value">{timeLeft.seconds}</div>
+          <div className="countdown__label">seconds</div>
+        </div>
+      </div>
+    </section>
+  )
+}
 
 export default Countdown
